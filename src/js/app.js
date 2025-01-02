097ed4215a88ce18724c640882a7f0bd42166c6a
ddoubledamage <47157358+ddoubledamage@users.noreply.github.com>
2025-01-02 21:14:04 +0300

    eslint fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 let collapsible = document.getElementsByClassName("collapse");
 
 for (let i = 0; i < collapsible.length; i++) {
-    collapsible[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let dropDownContainer = this.nextElementSibling;
-        if (dropDownContainer.classList.contains("visible")) {
-            dropDownContainer.classList.remove("visible");
-        } else {
-            dropDownContainer.classList.add("visible");
-        }
-    })
+  collapsible[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let dropDownContainer = this.nextElementSibling;
+    if (dropDownContainer.classList.contains("visible")) {
+      dropDownContainer.classList.remove("visible");
+    } else {
+      dropDownContainer.classList.add("visible");
+    }
+  });
 }
